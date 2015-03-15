8dd8bbab190b9986066c4ce3066b553b7039491e
Vectrex33 <vect33@gmail.com>
2015-03-15 08:42:24 -0700

    Update mainvectrextestcase.py
    Just Cleaning up my code...

--- testcases/mainvectrextestcase.py
+++ testcases/mainvectrextestcase.py
@@ -1,9 +1,9 @@
 # Imports the web browser package, used for opening sites.
 import webbrowser
 # These are the definitions for the urls.
-url=("http://www.youtube.com")
+url="http://www.youtube.com"
 url3="https://www.youtube.com/user/PewDiePie"
-url2=("http://google.ca")
+url2="http://google.ca"
 # Then these nex commands would run the url's above.
 # webbrowser.open(url)
 # webbrowser.open(url2)
