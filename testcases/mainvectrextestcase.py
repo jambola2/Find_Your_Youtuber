bcc97843b6ecef7cd0c9b96bf3e4e2053c5365e7
Vectrex33 <vect33@gmail.com>
2015-03-15 08:54:02 -0700

    Update mainvectrextestcase.py
    There we go.

--- testcases/mainvectrextestcase.py
+++ testcases/mainvectrextestcase.py
@@ -1,11 +1,5 @@
 # Imports the web browser package, used for opening sites.
 import webbrowser
-# These are the definitions for the urls.
-url="http://www.youtube.com"
-url3="https://www.youtube.com/user/PewDiePie"
-url2="http://google.ca"
-# Then these nex commands would run the url's above.
-# webbrowser.open(url)
-# webbrowser.open(url2)
-# ^ These were intentionally left as comments. This next one will excecute the link to pewdiepie's channel.
-webbrowser.open(url3)
+# Just found out you do not need to have definitions for the url's,
+# Just gotta put the url in quotation marks.
+webbrowser.open("http://coinchat.org")
