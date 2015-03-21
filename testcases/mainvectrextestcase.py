315c8fa4c00ec04c7bdcb1b942c7bcc3624cad36
Austin <austincgomez@gmail.com>
2015-03-21 04:22:15 -0500

    Update mainvectrextestcase.py

--- testcases/mainvectrextestcase.py
+++ testcases/mainvectrextestcase.py
@@ -1,3 +1,5 @@
+#Vectrex please message me on Coinchat we need to discuss more on implemention of the below code as I am confused thanks 
+
 # Imports the web browser package, used for opening sites.
 import webbrowser
 # Just found out you do not need to have definitions for the url's,
